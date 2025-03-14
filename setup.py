89ee6aea8426c9d9103fcf5e5d2e3a3468db079e
cazzysoci <carielglobio@gmail.com>
2025-03-14 17:48:19 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+os.system('npm install axios')
+os.system('npm install node-fetch')
+os.system('npm install proxy-agent')
+print(f"\x1b[31m[\x1b[33mCazddos\x1b[31m]\x1b[0m \x1b[32m> \x1b[33mInstallation Successful")
