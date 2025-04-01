b38037212176bb68fea4da26196f28d869bcf274
cazzysoci <carielglobio@gmail.com>
2025-04-01 21:35:20 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ os.system('npm install net')
 os.system('npm install random')
 os.system('npm install fs')
 os.system('npm install time')
+os.system('npm install cloudscraper randomstring request')
 os.system('node index.js')
 
 print(f"\x1b[31m[\x1b[33mCazddos\x1b[31m]\x1b[0m \x1b[32m> \x1b[33mInstallation Successful")
